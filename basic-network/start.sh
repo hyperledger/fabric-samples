a7e83fc23594ab22516bd6ae5d834f7336100825
Gari Singh <gari.r.singh@gmail.com>
2017-07-20 08:24:26 -0400

    FAB-5392 Fix path issue for Git Bash users
    Git Bash is a prereq for Windows users
    required to run the various sample
    scripts.  Git Bash by default will
    convert paths and this causes the
    issue identified by FAB-5392.
    
    Luckily Git Bash provides supports an
    environment variable to override this
    behavior.
    
    Change-Id: I66e52eb2953c0dcc9f04903a1fd1fd94d19b75ce
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -7,6 +7,9 @@
 # Exit on first error, print all commands.
 set -ev
 
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
 docker-compose -f docker-compose.yml down
 
 docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
