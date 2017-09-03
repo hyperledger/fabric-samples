57f3c1165185542a239bbea226199dfa074653bf
lezwon <lezwon@gmail.com>
2017-09-03 13:05:34 +0530

    Added quotes to $PWD Without quotes the shell executables fail to run, when the directory path has spaces in Windows

--- fabcar/startFabric.sh
+++ fabcar/startFabric.sh
@@ -15,7 +15,7 @@ starttime=$(date +%s)
 if [ ! -d ~/.hfc-key-store/ ]; then
 	mkdir ~/.hfc-key-store/
 fi
-cp $PWD/creds/* ~/.hfc-key-store/
+cp "$PWD"/creds/* ~/.hfc-key-store/
 # launch network; create channel and join peer to channel
 cd ../basic-network
 ./start.sh
