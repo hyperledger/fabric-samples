cba57da0dc08942448f6551ebf651db00ca84601
Surya <suryalnvs@gmail.com>
2018-02-22 12:28:40 -0500

    [FAB-8165] Adding upgrade function to byfn
    Adding upgrade functionality to byfn.sh to upgrade the fabric network
    from v1.0.x to v1.1 and scripts for cli to enable
    /Channel, /Channel/Orderer, /Channel/Application capabilities
    
    Usage:
    git fetch origin
    git checkout origin/release
    ./byfn.sh -m up -i 1.0.6
    git checkout origin/master
    ./byfn.sh upgrade
    
    Change-Id: I6f53a6db39501e2653dc4c325d3d42d78f463e87
    Signed-off-by: Surya <suryalnvs@gmail.com>
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- first-network/scripts/utils.sh
+++ first-network/scripts/utils.sh
@@ -17,6 +17,13 @@ verifyResult () {
 	fi
 }
 
+# Set OrdererOrg.Admin globals
+setOrdererGlobals() {
+        CORE_PEER_LOCALMSPID="OrdererMSP"
+        CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+        CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/users/Admin@example.com/msp
+}
+
 setGlobals () {
 	PEER=$1
 	ORG=$2
@@ -169,6 +176,50 @@ chaincodeQuery () {
 	echo
 	exit 1
   fi
+}
+
+# fetchChannelConfig <channel_id> <output_json>
+# Writes the current channel config for a given channel to a JSON file
+fetchChannelConfig() {
+  CHANNEL=$1
+  OUTPUT=$2
+
+  setOrdererGlobals
+
+  echo "Fetching the most recent configuration block for the channel"
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    peer channel fetch config config_block.pb -o orderer.example.com:7050 -c $CHANNEL --cafile $ORDERER_CA
+  else
+    peer channel fetch config config_block.pb -o orderer.example.com:7050 -c $CHANNEL --tls --cafile $ORDERER_CA
+  fi
+
+  echo "Decoding config block to JSON and isolating config to ${OUTPUT}"
+  configtxlator proto_decode --input config_block.pb --type common.Block | jq .data.data[0].payload.data.config > "${OUTPUT}"
+}
+
+# signConfigtxAsPeerOrg <org> <configtx.pb>
+# Set the peerOrg admin of an org and signing the config update
+signConfigtxAsPeerOrg() {
+        PEERORG=$1
+        TX=$2
+        setGlobals 0 $PEERORG
+        peer channel signconfigtx -f "${TX}"
+}
+
+# createConfigUpdate <channel_id> <original_config.json> <modified_config.json> <output.pb>
+# Takes an original and modified config, and produces the config update tx which transitions between the two
+createConfigUpdate() {
+  CHANNEL=$1
+  ORIGINAL=$2
+  MODIFIED=$3
+  OUTPUT=$4
+
+  configtxlator proto_encode --input "${ORIGINAL}" --type common.Config > original_config.pb
+  configtxlator proto_encode --input "${MODIFIED}" --type common.Config > modified_config.pb
+  configtxlator compute_update --channel_id "${CHANNEL}" --original original_config.pb --updated modified_config.pb > config_update.pb
+  configtxlator proto_decode --input config_update.pb  --type common.ConfigUpdate > config_update.json
+  echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . > config_update_in_envelope.json
+  configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope > "${OUTPUT}"
 }
 
 chaincodeInvoke () {
