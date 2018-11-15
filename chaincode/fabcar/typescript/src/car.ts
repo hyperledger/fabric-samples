4fb3b57e6b6ed8a977fac679d5d512386b32c7c7
Simon Stone <sstone1@uk.ibm.com>
2018-11-15 10:44:02 +0000

    [FAB-12852] Add fabcar contract w/ new prog model (TS)
    Add new FabCar contract sample using the new programming model, written
    in TypeScript. Add the new contract sample alongside the existing one
    written in the old programming model, which will be removed in a subsequent
    change request.
    
    Change-Id: I47e5f16b475c68f9a1c4752af2623f090fbf12c9
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

---
+++ chaincode/fabcar/typescript/src/car.ts
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export class Car {
+    public docType?: string;
+    public color: string;
+    public make: string;
+    public model: string;
+    public owner: string;
+}
