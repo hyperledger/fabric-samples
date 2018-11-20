c18419644d072056a32c64cea2938a032a75f429
Simon Stone <sstone1@uk.ibm.com>
2018-11-20 16:01:36 +0000

    [FAB-12878] Add fabcar app using new prog model (TS)
    Add a new version of the FabCar applications using the new
    programming model (fabric-network), written in TypeScript.
    
    The new programming model requires a connection profile, so
    I have added connection profiles (JSON and YAML) versions to
    the basic-network sample. When we switch FabCar to use first-network,
    we will have to add connection profiles into that as well.
    
    Change-Id: I81bae907fc64a1cde0234325f37b53e5cd7964e5
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

---
+++ fabcar/typescript/src/enrollAdmin.ts
@@ -0,0 +1,45 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as FabricCAServices from 'fabric-ca-client';
+import { FileSystemWallet, X509WalletMixin } from 'fabric-network';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const ccpPath = path.resolve(__dirname, '..', '..', '..', 'basic-network', 'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+async function main() {
+    try {
+
+        // Create a new CA client for interacting with the CA.
+        const caURL = ccp.certificateAuthorities['ca.example.com'].url;
+        const ca = new FabricCAServices(caURL);
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the admin user.
+        const adminExists = await wallet.exists('admin');
+        if (adminExists) {
+            console.log('An identity for the admin user "admin" already exists in the wallet');
+            return;
+        }
+
+        // Enroll the admin user, and import the new identity into the wallet.
+        const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
+        const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
+        wallet.import('admin', identity);
+        console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
+
+    } catch (error) {
+        console.error(`Failed to enroll admin user "admin": ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
