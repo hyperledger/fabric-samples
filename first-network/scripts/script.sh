c7438e1f7c03be5310286ffc5f22f9da615f022b
Arnaud J Le Hors <lehors@us.ibm.com>
2019-01-14 15:25:28 +0100

    [FAB-13668] BYFN's container volume mapping is bad
    Updated compose files and scripts to use the correct mapping.
    
    Change-Id: Iccbe384393c2736c5ba197d70ef6a124588ad588
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

--- first-network/scripts/script.sh
+++ first-network/scripts/script.sh
@@ -23,13 +23,13 @@ LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
 COUNTER=1
 MAX_RETRY=10
 
-CC_SRC_PATH="github.com/chaincode/chaincode_example02/go/"
+CC_SRC_PATH="github.com/hyperledger/fabric-samples/chaincode/chaincode_example02/go/"
 if [ "$LANGUAGE" = "node" ]; then
-	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/chaincode_example02/node/"
 fi
 
 if [ "$LANGUAGE" = "java" ]; then
-	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/java/"
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/chaincode_example02/java/"
 fi
 
 echo "Channel name : "$CHANNEL_NAME
--- first-network/scripts/step3org3.sh
+++ first-network/scripts/step3org3.sh
@@ -29,9 +29,9 @@ LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
 COUNTER=1
 MAX_RETRY=5
 
-CC_SRC_PATH="github.com/chaincode/chaincode_example02/go/"
+CC_SRC_PATH="github.com/hyperledger/fabric-samples/chaincode/chaincode_example02/go/"
 if [ "$LANGUAGE" = "node" ]; then
-	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/chaincode_example02/node/"
 fi
 
 # import utils
