8245252acc61c2d690220a724f43d6c1bbfe75cc
wenjian3 <wenjianq@gmail.com>
2019-03-21 20:04:46 -0400

    [FABN-1184] Implement fabtoken sample app
    - implement sample app to issue, transfer, redeem and list tokens
    - change basic-network/crypto-config.yaml user count to 2
    - regenerate crypto and channel config via generate.sh
    - add fabtoken/README.md
    
    Change-Id: I8d270c95b29e4af9c432fb05e7e8dc6be7bbc069
    Signed-off-by: Wenjian Qiao <wenjianq@gmail.com>

---
+++ fabtoken/startFabric.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error
+set -e
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+starttime=$(date +%s)
+
+# launch network; create channel and join peer to channel
+cd ../basic-network
+./start.sh
+
+cat <<EOF
+
+Total setup execution time : $(($(date +%s) - starttime)) secs ...
+
+Next, use the FabToken application to interact with the Fabric network.
+
+  Start by changing into the "javascript" directory:
+    cd javascript
+
+  Next, install all required packages:
+    npm install
+
+  Then run the fabtoken application to perform the token operations.
+
+    node fabtoken
+      - when no argument is passed, it will run a demo with predefined token operations
+    node fabtoken issue <username> <token_type> <quantity>
+      - example 1: node fabtoken issue user1 USD 100
+    node fabtoken list <username>
+      - example: node fabtoken list user1
+      - select a token to transfer or redeem and pass "tx_id" and "index" as input parameters
+    node fabtoken transfer <from_user> <to_user> <quantity> <tx_id> <index>
+      - example: node fabtoken transfer user1 user2 30 c9b1211d9ad809e6ee1b542de6886d8d1d9e1c938d88eff23a3ddb4e8c080e4d 0
+      - <tx_id> and <index> are the "tx_id" and "index" returned from the list operation that specifies the token id for transfer
+    node fabtoken redeem <username> <quantity> <tx_id> <index>
+      - example: node fabtoken redeem user2 10 477c7bf2002814497c228fd8cbc4d80c8b7f1602b2c17ffadb6cf7e5783fa47a 0
+      - <tx_id> and <index> are the "tx_id" and "index" returned from the list operation
+
+EOF
