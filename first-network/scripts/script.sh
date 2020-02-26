53f522078819ff56e3d07119a29cd01a92f3e72e
Brett Logan <brett.t.logan@ibm.com>
2020-02-25 21:22:01 -0500

    Revert "first-network/scripts/*: Make Chaincode name configurable (#118)"
    This reverts commit 9ef61e2d7204964ad02b53b171c05f87a673e42d.
    
    Introduced regressions when running without explicitly specifying
    a chaincode label
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- first-network/scripts/script.sh
+++ first-network/scripts/script.sh
@@ -15,14 +15,12 @@ CC_SRC_LANGUAGE="$3"
 TIMEOUT="$4"
 VERBOSE="$5"
 NO_CHAINCODE="$6"
-NAME="$7"
 : ${CHANNEL_NAME:="mychannel"}
 : ${DELAY:="3"}
 : ${CC_SRC_LANGUAGE:="go"}
 : ${TIMEOUT:="10"}
 : ${VERBOSE:="false"}
 : ${NO_CHAINCODE:="false"}
-: ${NAME:="mycc"}
 CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
 COUNTER=1
 MAX_RETRY=20
@@ -45,7 +43,6 @@ fi
 
 
 echo "Channel name : "$CHANNEL_NAME
-echo "Chaincode name : "$NAME
 
 # import utils
 . scripts/utils.sh
