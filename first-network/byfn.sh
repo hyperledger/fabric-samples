14d90a5e977b9c7b1f1250ecd2b92eb3d983d527
KIM GEONWOO <kgw8919@gmail.com>
2020-03-25 17:50:28 +0900

    [FAB-17656] echo Generating channel.tx echo Generating channel configuration transaction 'channel.tx' it will be output to the console "configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel"
    Signed-off-by: KIM GEONWOO <kgw8919@gmail.com>

--- first-network/byfn.sh
+++ first-network/byfn.sh
@@ -371,8 +371,10 @@ function generateChannelArtifacts() {
   echo "##########################################################"
   # Note: For some unknown reason (at least for now) the block file can't be
   # named orderer.genesis.block or the orderer will fail to launch!
+  set -x
   configtxgen -profile SampleMultiNodeEtcdRaft -channelID byfn-sys-channel -outputBlock ./channel-artifacts/genesis.block
   res=$?
+  set +x
   if [ $res -ne 0 ]; then
     echo "Failed to generate orderer genesis block..."
     exit 1
