edd17e7a680bcbfe81f2966d005df83fc1105970
NIKHIL E GUPTA <negupta@us.ibm.com>
2020-03-25 16:44:14 -0400

    Add enrollUser files to commercial paper
    Signed-off-by: NIKHIL E GUPTA <negupta@us.ibm.com>

--- commercial-paper/network-starter.sh
+++ commercial-paper/network-starter.sh
@@ -20,11 +20,17 @@ cd "${DIR}/../test-network/"
 
 docker kill cliDigiBank cliMagnetoCorp logspout || true
 ./network.sh down
-./network.sh up createChannel -s couchdb -i 2.0.0
+./network.sh up createChannel -ca -s couchdb
 
-# Copy the connection profiles so they are in the correct organizations. 
+# Copy the connection profiles so they are in the correct organizations.
 cp "${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/connection-org1.yaml" "${DIR}/organization/digibank/gateway/"
 cp "${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/connection-org2.yaml" "${DIR}/organization/magnetocorp/gateway/"
+
+cp ${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/* ${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem
+cp ${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/* ${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk
+
+cp ${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/* ${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/User1@org2.example.com-cert.pem
+cp ${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/* ${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/priv_sk
 
 echo Suggest that you monitor the docker containers by running
 echo "./organization/magnetocorp/configuration/cli/monitordocker.sh net_test"
