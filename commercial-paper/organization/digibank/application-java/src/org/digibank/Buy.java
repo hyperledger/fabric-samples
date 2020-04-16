a7034cff6ae9568c736e3d702e14f8d498d7109a
linqili <charlielin06@gmail.com>
2020-04-17 00:52:35 +0800

    making commercial-paper application-java to fit 2.0.0
    Signed-off-by: linqili <charlielin06@gmail.com>

--- commercial-paper/organization/digibank/application-java/src/org/digibank/Buy.java
+++ commercial-paper/organization/digibank/application-java/src/org/digibank/Buy.java
@@ -15,12 +15,13 @@ import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.GatewayException;
 import org.hyperledger.fabric.gateway.Network;
 import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
 import org.papernet.CommercialPaper;
 
 public class Buy {
 
 	private static final String ENVKEY="CONTRACT_NAME";
-	
+
 	public static void main(String[] args) {
 		Gateway.Builder builder = Gateway.createBuilder();
 
@@ -33,28 +34,29 @@ public class Buy {
 
 		try {
 			// A wallet stores a collection of identities
-			Path walletPath = Paths.get("..", "identity", "user", "balaji", "wallet");
-			Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+			Path walletPath = Paths.get(".", "wallet");
+			Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+			System.out.println("Read wallet info from: " + walletPath);
 
-			String userName = "Admin@org1.example.com";
+			String userName = "User1@org1.example.com";
 
-			Path connectionProfile = Paths.get("..", "gateway", "networkConnection.yaml");
+			Path connectionProfile = Paths.get("..", "gateway", "connection-org1.yaml");
 
-		    // Set connection options on the gateway builder
+			// Set connection options on the gateway builder
 			builder.identity(wallet, userName).networkConfig(connectionProfile).discovery(false);
 
-		    // Connect to gateway using application specified parameters
+			// Connect to gateway using application specified parameters
 			try(Gateway gateway = builder.connect()) {
 
 				// Access PaperNet network
-			    System.out.println("Use network channel: mychannel.");
-			    Network network = gateway.getNetwork("mychannel");
+				System.out.println("Use network channel: mychannel.");
+				Network network = gateway.getNetwork("mychannel");
 
-			    // Get addressability to commercial paper contract
-			    System.out.println("Use org.papernet.commercialpaper smart contract.");
-			    Contract contract = network.getContract(contractName, "org.papernet.commercialpaper");
+				// Get addressability to commercial paper contract
+				System.out.println("Use org.papernet.commercialpaper smart contract.");
+				Contract contract = network.getContract(contractName, "org.papernet.commercialpaper");
 
-			    // Buy commercial paper
+				// Buy commercial paper
 				System.out.println("Submit commercial paper buy transaction.");
 				byte[] response = contract.submitTransaction("buy", "MagnetoCorp", "00001", "MagnetoCorp", "DigiBank", "4900000", "2020-05-31");
 
--- commercial-paper/organization/digibank/application-java/src/org/digibank/Redeem.java
+++ commercial-paper/organization/digibank/application-java/src/org/digibank/Redeem.java
@@ -15,15 +15,16 @@ import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.GatewayException;
 import org.hyperledger.fabric.gateway.Network;
 import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
 import org.papernet.CommercialPaper;
 
 public class Redeem {
 
 	private static final String ENVKEY="CONTRACT_NAME";
-	
+
 	public static void main(String[] args) {
 		Gateway.Builder builder = Gateway.createBuilder();
-		
+
 		String contractName="papercontract";
 		// get the name of the contract, in case it is overridden
 		Map<String,String> envvar = System.getenv();
@@ -33,28 +34,28 @@ public class Redeem {
 
 		try {
 			// A wallet stores a collection of identities
-			Path walletPath = Paths.get("..",  "identity", "user", "balaji", "wallet");
-			Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+			Path walletPath = Paths.get(".", "wallet");
+			Wallet wallet = Wallets.newFileSystemWallet(walletPath);
 
-			String userName = "Admin@org1.example.com";
+			String userName = "User1@org1.example.com";
 
-			Path connectionProfile = Paths.get("..", "gateway", "networkConnection.yaml");
+			Path connectionProfile = Paths.get("..", "gateway", "connection-org1.yaml");
 
-		    // Set connection options on the gateway builder
+			// Set connection options on the gateway builder
 			builder.identity(wallet, userName).networkConfig(connectionProfile).discovery(false);
 
-		    // Connect to gateway using application specified parameters
+			// Connect to gateway using application specified parameters
 			try(Gateway gateway = builder.connect()) {
 
 				// Access PaperNet network
-			    System.out.println("Use network channel: mychannel.");
-			    Network network = gateway.getNetwork("mychannel");
+				System.out.println("Use network channel: mychannel.");
+				Network network = gateway.getNetwork("mychannel");
 
-			    // Get addressability to commercial paper contract
-			    System.out.println("Use org.papernet.commercialpaper smart contract.");
-			    Contract contract = network.getContract("papercontract", "org.papernet.commercialpaper");
+				// Get addressability to commercial paper contract
+				System.out.println("Use org.papernet.commercialpaper smart contract.");
+				Contract contract = network.getContract("papercontract", "org.papernet.commercialpaper");
 
-			    // Redeem commercial paper
+				// Redeem commercial paper
 				System.out.println("Submit commercial paper redeem transaction.");
 				byte[] response = contract.submitTransaction("redeem", "MagnetoCorp", "00001", "DigiBank", "2020-11-30");
 
