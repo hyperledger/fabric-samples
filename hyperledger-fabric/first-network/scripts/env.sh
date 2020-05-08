ca7ba771ec525ff6e01c1a07773921f1b7be96d3
ccorley <ccorley@us.ibm.com>
2020-05-08 17:28:36 -0500

    Adding readme and env script

---
+++ hyperledger-fabric/first-network/scripts/env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export CHANNEL_NAME=channel1
+export CAFILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export ORG1TLS=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export ORG2TLS=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+
