96fcc204995975e2e2f31f0b700d7f96f8c327dd
Chongxin Luo <Chongxin.Luo@ibm.com>
2020-06-16 15:42:13 -0400

    Adding chaincode-typescript to asset-transfer-basic sample.
    Signed-off-by: Chongxin Luo <Chongxin.Luo@ibm.com>

---
+++ asset-transfer-basic/chaincode-typescript/src/asset.ts
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export class Asset {
+    public docType?: string;
+    public ID: string;
+    public Color: string;
+    public Size: number;
+    public Owner: string;
+    public AppraisedValue: number;
+}
