02bad1de919e7d7feae47b659ddad0cdb43851e8
Brett Logan <brett.t.logan@ibm.com>
2020-07-15 14:14:31 -0400

    Update CI For Asset Transfer
    Pushes the scripting logic into a standalone script
    and adds tests for the applications which by side-effect
    also test the invoke and query functions on the chaincode.
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

---
+++ ci/scripts/run-test-network-private.sh
@@ -0,0 +1,21 @@
+set -euo pipefail
+
+FABRIC_VERSION=${FABRIC_VERSION:-2.2}
+CHAINCODE_LANGUAGE=${CHAINCODE_LANGUAGE:-go}
+CHAINCODE_NAME=${CHAINCODE_NAME:-private}
+
+function print() {
+	GREEN='\033[0;32m'
+  NC='\033[0m'
+  echo
+	echo -e "${GREEN}${1}${NC}"
+}
+
+print "Creating network"
+./network.sh up createChannel -ca -s couchdb -i "${FABRIC_VERSION}"
+
+print "Deploying ${CHAINCODE_NAME} chaincode"
+./network.sh deployCC -ccn "${CHAINCODE_NAME}" -ccv 1 -ccs 1 -ccl "${CHAINCODE_LANGUAGE}"
+
+print "Stopping network"
+./network.sh down
