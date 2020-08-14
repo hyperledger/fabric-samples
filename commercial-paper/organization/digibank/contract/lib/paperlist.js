5eea27d295b440768c476708d0f143c4004e7753
mahoney@uk.ibm.com <mahoney@uk.ibm.com>
2020-08-14 15:51:41 +0100

    Commercial Paper enhancements and new README
    Signed-off-by: Paul O'M <mahoney@uk.ibm.com>

--- commercial-paper/organization/digibank/contract/lib/paperlist.js
+++ commercial-paper/organization/digibank/contract/lib/paperlist.js
@@ -14,7 +14,7 @@ const CommercialPaper = require('./paper.js');
 class PaperList extends StateList {
 
     constructor(ctx) {
-        super(ctx, 'org.papernet.commercialpaperlist');
+        super(ctx, 'org.papernet.paper');
         this.use(CommercialPaper);
     }
 
--- commercial-paper/organization/magnetocorp/contract/ledger-api/state.js
+++ commercial-paper/organization/magnetocorp/contract/ledger-api/state.js
@@ -38,9 +38,10 @@ class State {
         return this.currentState;
     }
 
-    serialize() {
+/*    serialize() {
+        
         return State.serialize(this);
-    }
+    } */
 
     /**
      * Convert object to buffer containing JSON data serialization
@@ -49,6 +50,8 @@ class State {
      * @return {buffer} buffer with the data to store
      */
     static serialize(object) {
+        // don't write the key:value passed in - we already have a real composite key, issuer and paper Number.
+        delete object.key;
         return Buffer.from(JSON.stringify(object));
     }
 
@@ -88,7 +91,8 @@ class State {
      * @param (String[]) keyParts
      */
     static makeKey(keyParts) {
-        return keyParts.map(part => JSON.stringify(part)).join(':');
+        // return keyParts.map(part => JSON.stringify(part)).join(':');
+        return keyParts.map(part => part).join(':');
     }
 
     static splitKey(key){
