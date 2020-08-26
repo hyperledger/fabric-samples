2eca551fa36be7b3b6fb723e35ebbc6e4a237941
Naser Mirzaei <nasermirzaei89@gmail.com>
2020-08-27 01:20:08 +0430

    add utils.sh and colorize messages
    Signed-off-by: Naser Mirzaei <nasermirzaei89@gmail.com>

---
+++ test-network/utils.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+C_RESET='\033[0m'
+C_RED='\033[0;31m'
+C_GREEN='\033[0;32m'
+C_BLUE='\033[0;34m'
+C_YELLOW='\033[1;33m'
+
+# println echos string
+function println() {
+  echo -e "$1"
+}
+
+# errorln echos i red color
+function errorln() {
+  println "${C_RED}${1}${C_RESET}"
+}
+
+# successln echos in green color
+function successln() {
+  println "${C_GREEN}${1}${C_RESET}"
+}
+
+# infoln echos in blue color
+function infoln() {
+  println "${C_BLUE}${1}${C_RESET}"
+}
+
+# warnln echos in yellow color
+function warnln() {
+  println "${C_YELLOW}${1}${C_RESET}"
+}
+
+# fatalln echos in red color and exits with fail status
+function fatalln() {
+  errorln "$1"
+  exit 1
+}
+
+export -f errorln
+export -f successln
+export -f infoln
+export -f warnln
