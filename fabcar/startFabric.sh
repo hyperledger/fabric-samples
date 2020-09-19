ef31973a0c0d1bdb4d7ae2ad8123982f2ccc2c4e
lengyijun <sjtu5140809011@gmail.com>
2020-09-19 12:10:27 +0800

    shellcheck fix
    Signed-off-by: lengyijun <sjtu5140809011@gmail.com>

--- fabcar/startFabric.sh
+++ fabcar/startFabric.sh
@@ -11,9 +11,9 @@ set -e
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 CC_SRC_LANGUAGE=${1:-"go"}
-CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
+CC_SRC_LANGUAGE=$(echo "$CC_SRC_LANGUAGE" | tr '[:upper:]' '[:lower:]')
 
-if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ] ; then
+if [ "$CC_SRC_LANGUAGE" = "go" ] -o [ "$CC_SRC_LANGUAGE" = "golang" ] ; then
 	CC_SRC_PATH="../chaincode/fabcar/go/"
 elif [ "$CC_SRC_LANGUAGE" = "javascript" ]; then
 	CC_SRC_PATH="../chaincode/fabcar/javascript/"
@@ -22,7 +22,7 @@ elif [ "$CC_SRC_LANGUAGE" = "java" ]; then
 elif [ "$CC_SRC_LANGUAGE" = "typescript" ]; then
 	CC_SRC_PATH="../chaincode/fabcar/typescript/"
 else
-	echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
+	echo The chaincode language "${CC_SRC_LANGUAGE}" is not supported by this script
 	echo Supported chaincode languages are: go, java, javascript, and typescript
 	exit 1
 fi
@@ -37,7 +37,7 @@ rm -rf go/wallet/*
 pushd ../test-network
 ./network.sh down
 ./network.sh up createChannel -ca -s couchdb
-./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
+./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl "${CC_SRC_LANGUAGE}" -ccp "${CC_SRC_PATH}"
 popd
 
 cat <<EOF
