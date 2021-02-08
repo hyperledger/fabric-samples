bbfb2db1a0a06a6cbc34a70dc063d6d06c3cc3da
Brett Logan <lindluni@github.com>
2021-02-08 11:18:28 -0500

    Use Dynamic Linter
    The new lint.sh script no longer requires the use
    of a matrix and instead dynamically finds and lints
    directories.
    
    Signed-off-by: Brett Logan <lindluni@github.com>

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -7,45 +7,47 @@ function print() {
 	echo -e "${GREEN}${1}${NC}"
 }
 
-if [[ "${LANGUAGE}" == "go" ]]; then
-  go get golang.org/x/tools/cmd/goimports
+dirs=("$(find . -name "*-go" -o -name "*-java" -o -name "*-javascript" -o -name "*-typescript")")
+for dir in $dirs; do
+  if [[ -d $dir ]]; then
+    print "Linting $dir"
+    pushd $dir
+    if [[ "$dir" =~ "-go" ]]; then
+      go get golang.org/x/tools/cmd/goimports
+      print "Running go vet"
+      go vet ./...
+      print "Running gofmt"
+      output=$(gofmt -l -s $(go list -f '{{.Dir}}' ./...))
+      if [[ "${output}" != "" ]]; then
+        print "The following files contain formatting errors, please run 'gofmt -l -w <path>' to fix these issues:"
+        echo "${output}"
+      fi
 
-  cd "${DIRECTORY}/${TYPE}-${LANGUAGE}"
-  print "Running go vet"
-  go vet ./...
-
-  print "Running gofmt"
-  output=$(gofmt -l -s $(go list -f '{{.Dir}}' ./...))
-  if [[ "${output}" != "" ]]; then
-    print "The following files contain formatting errors, please run 'gofmt -l -w <path>' to fix these issues:"
-    echo "${output}"
+      print "Running goimports"
+      output=$(goimports -l $(go list -f '{{.Dir}}' ./...))
+      if [[ "${output}" != "" ]]; then
+        print "The following files contain import errors, please run 'goimports -l -w <path>' to fix these issues:"
+        echo "${output}"
+      fi
+    elif [[ "$dir" =~ "-javascript" ]]; then
+      print "Running ESLint"
+      if [[ "$dir" =~ "chaincode" ]]; then
+        eslint *.js */**.js
+      else
+        eslint *.js
+      fi
+    elif [[ "$dir" =~ "-java" ]]; then
+      if [[ -f "pom.xml" ]]; then
+        print "Running Maven Build"
+        mvn clean package
+      else
+        print "Running Gradle Build"
+        ./gradlew build
+      fi
+    elif [[ "$dir" =~ "-typescript" ]]; then
+      print "Running TSLint"
+      tslint --project .
+    fi
+    popd
   fi
-
-  print "Running goimports"
-  output=$(goimports -l $(go list -f '{{.Dir}}' ./...))
-  if [[ "${output}" != "" ]]; then
-    print "The following files contain import errors, please run 'goimports -l -w <path>' to fix these issues:"
-    echo "${output}"
-  fi
-elif [[ "${LANGUAGE}" == "java" ]]; then
-  cd "${DIRECTORY}/${TYPE}-${LANGUAGE}"
-  print "Running Gradle Build"
-  ./gradlew build
-elif [[ "${LANGUAGE}" == "javascript" ]]; then
-  npm install -g eslint
-  cd "${DIRECTORY}/${TYPE}-${LANGUAGE}"
-  print "Running ESLint"
-  if [[ "${TYPE}" == "chaincode" ]]; then
-    eslint *.js */**.js
-  else
-    eslint *.js
-  fi
-elif [[ "${LANGUAGE}" == "typescript" ]]; then
-  npm install -g typescript tslint
-  cd "${DIRECTORY}/${TYPE}-${LANGUAGE}"
-  print "Running TSLint"
-  tslint --project .
-else
-  echo "Language not supported"
-  exit 1
-fi
+done
