6726e77932f52f22747910e3d6a7e634e44c7c40
Brett Logan <lindluni@github.com>
2021-02-08 09:39:11 -0500

    Add ESLinte to Auction Example
    The auction example had no ESLint configuration.
    This change adds a .eslintrc.js file that matches
    the rest of the projects.
    
    This change also fixes all the linting issue in
    the auction example.
    
    Signed-off-by: Brett Logan <lindluni@github.com>

--- auction/application-javascript/queryAuction.js
+++ auction/application-javascript/queryAuction.js
@@ -14,62 +14,62 @@ const myChannel = 'mychannel';
 const myChaincodeName = 'auction';
 
 async function queryAuction(ccp,wallet,user,auctionID) {
-    try {
+	try {
 
-        const gateway = new Gateway();
-      //connect using Discovery enabled
+		const gateway = new Gateway();
+		//connect using Discovery enabled
 
-      await gateway.connect(ccp,
-          { wallet: wallet, identity: user, discovery: { enabled: true, asLocalhost: true } });
+		await gateway.connect(ccp,
+			{ wallet: wallet, identity: user, discovery: { enabled: true, asLocalhost: true } });
 
-        const network = await gateway.getNetwork(myChannel);
-        const contract = network.getContract(myChaincodeName);
+		const network = await gateway.getNetwork(myChannel);
+		const contract = network.getContract(myChaincodeName);
 
-        console.log('\n--> Evaluate Transaction: query the auction');
-        let result = await contract.evaluateTransaction('QueryAuction',auctionID);
-        console.log('*** Result: Auction: ' + prettyJSONString(result.toString()));
+		console.log('\n--> Evaluate Transaction: query the auction');
+		let result = await contract.evaluateTransaction('QueryAuction',auctionID);
+		console.log('*** Result: Auction: ' + prettyJSONString(result.toString()));
 
-        gateway.disconnect();
-    } catch (error) {
-        console.error(`******** FAILED to submit bid: ${error}`);
+		gateway.disconnect();
+	} catch (error) {
+		console.error(`******** FAILED to submit bid: ${error}`);
 	}
 }
 
 async function main() {
-    try {
+	try {
 
-        if (process.argv[2] == undefined || process.argv[3] == undefined
-            || process.argv[4] == undefined) {
-            console.log("Usage: node queryAuction.js org userID auctionID");
-            process.exit(1);
-        }
+		if (process.argv[2] == undefined || process.argv[3] == undefined ||
+            process.argv[4] == undefined) {
+			console.log('Usage: node queryAuction.js org userID auctionID');
+			process.exit(1);
+		}
 
-        const org = process.argv[2];
-        const user = process.argv[3];
-        const auctionID = process.argv[4];
+		const org = process.argv[2];
+		const user = process.argv[3];
+		const auctionID = process.argv[4];
 
-        if (org == 'Org1' || org == 'org1') {
+		if (org == 'Org1' || org == 'org1') {
 
-            const orgMSP = 'Org1MSP';
-            const ccp = buildCCPOrg1();
-            const walletPath = path.join(__dirname, 'wallet/org1');
-            const wallet = await buildWallet(Wallets, walletPath);
-            await queryAuction(ccp,wallet,user,auctionID);
-        }
-        else if (org == 'Org2' || org == 'org2') {
+			const orgMSP = 'Org1MSP';
+			const ccp = buildCCPOrg1();
+			const walletPath = path.join(__dirname, 'wallet/org1');
+			const wallet = await buildWallet(Wallets, walletPath);
+			await queryAuction(ccp,wallet,user,auctionID);
+		}
+		else if (org == 'Org2' || org == 'org2') {
 
-            const orgMSP = 'Org2MSP';
-            const ccp = buildCCPOrg2();
-            const walletPath = path.join(__dirname, 'wallet/org2');
-            const wallet = await buildWallet(Wallets, walletPath);
-            await queryAuction(ccp,wallet,user,auctionID);
-        }  else {
-            console.log("Usage: node queryAuction.js org userID auctionID");
-            console.log("Org must be Org1 or Org2");
-          }
-    } catch (error) {
+			const orgMSP = 'Org2MSP';
+			const ccp = buildCCPOrg2();
+			const walletPath = path.join(__dirname, 'wallet/org2');
+			const wallet = await buildWallet(Wallets, walletPath);
+			await queryAuction(ccp,wallet,user,auctionID);
+		}  else {
+			console.log('Usage: node queryAuction.js org userID auctionID');
+			console.log('Org must be Org1 or Org2');
+		}
+	} catch (error) {
 		console.error(`******** FAILED to run the application: ${error}`);
-    }
+	}
 }
 
 
--- auction/application-javascript/queryBid.js
+++ auction/application-javascript/queryBid.js
@@ -14,63 +14,63 @@ const myChannel = 'mychannel';
 const myChaincodeName = 'auction';
 
 async function queryBid(ccp,wallet,user,auctionID,bidID) {
-    try {
+	try {
 
-        const gateway = new Gateway();
-      //connect using Discovery enabled
+		const gateway = new Gateway();
+		//connect using Discovery enabled
 
-      await gateway.connect(ccp,
-          { wallet: wallet, identity: user, discovery: { enabled: true, asLocalhost: true } });
+		await gateway.connect(ccp,
+			{ wallet: wallet, identity: user, discovery: { enabled: true, asLocalhost: true } });
 
-        const network = await gateway.getNetwork(myChannel);
-        const contract = network.getContract(myChaincodeName);
+		const network = await gateway.getNetwork(myChannel);
+		const contract = network.getContract(myChaincodeName);
 
-        console.log('\n--> Evaluate Transaction: read bid from private data store');
-        let result = await contract.evaluateTransaction('QueryBid',auctionID,bidID);
-        console.log('*** Result: Bid: ' + prettyJSONString(result.toString()));
+		console.log('\n--> Evaluate Transaction: read bid from private data store');
+		let result = await contract.evaluateTransaction('QueryBid',auctionID,bidID);
+		console.log('*** Result: Bid: ' + prettyJSONString(result.toString()));
 
-        gateway.disconnect();
-    } catch (error) {
-        console.error(`******** FAILED to submit bid: ${error}`);
+		gateway.disconnect();
+	} catch (error) {
+		console.error(`******** FAILED to submit bid: ${error}`);
 	}
 }
 
 async function main() {
-    try {
+	try {
 
-        if (process.argv[2] == undefined || process.argv[3] == undefined
-            || process.argv[4] == undefined || process.argv[5] == undefined) {
-            console.log("Usage: node bid.js org userID auctionID bidID");
-            process.exit(1);
-        }
+		if (process.argv[2] == undefined || process.argv[3] == undefined ||
+            process.argv[4] == undefined || process.argv[5] == undefined) {
+			console.log('Usage: node bid.js org userID auctionID bidID');
+			process.exit(1);
+		}
 
-        const org = process.argv[2];
-        const user = process.argv[3];
-        const auctionID = process.argv[4];
-        const bidID = process.argv[5];
+		const org = process.argv[2];
+		const user = process.argv[3];
+		const auctionID = process.argv[4];
+		const bidID = process.argv[5];
 
-        if (org == 'Org1' || org == 'org1') {
+		if (org == 'Org1' || org == 'org1') {
 
-            const orgMSP = 'Org1MSP';
-            const ccp = buildCCPOrg1();
-            const walletPath = path.join(__dirname, 'wallet/org1');
-            const wallet = await buildWallet(Wallets, walletPath);
-            await queryBid(ccp,wallet,user,auctionID,bidID);
-        }
-        else if (org == 'Org2' || org == 'org2') {
+			const orgMSP = 'Org1MSP';
+			const ccp = buildCCPOrg1();
+			const walletPath = path.join(__dirname, 'wallet/org1');
+			const wallet = await buildWallet(Wallets, walletPath);
+			await queryBid(ccp,wallet,user,auctionID,bidID);
+		}
+		else if (org == 'Org2' || org == 'org2') {
 
-            const orgMSP = 'Org2MSP';
-            const ccp = buildCCPOrg2();
-            const walletPath = path.join(__dirname, 'wallet/org2');
-            const wallet = await buildWallet(Wallets, walletPath);
-            await queryBid(ccp,wallet,user,auctionID,bidID);
-        } else {
-            console.log("Usage: node bid.js org userID auctionID bidID");
-            console.log("Org must be Org1 or Org2");
-          }
-    } catch (error) {
-        console.error(`******** FAILED to run the application: ${error}`);
-    }
+			const orgMSP = 'Org2MSP';
+			const ccp = buildCCPOrg2();
+			const walletPath = path.join(__dirname, 'wallet/org2');
+			const wallet = await buildWallet(Wallets, walletPath);
+			await queryBid(ccp,wallet,user,auctionID,bidID);
+		} else {
+			console.log('Usage: node bid.js org userID auctionID bidID');
+			console.log('Org must be Org1 or Org2');
+		}
+	} catch (error) {
+		console.error(`******** FAILED to run the application: ${error}`);
+	}
 }
 
 
