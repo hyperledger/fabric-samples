7b883bb2988a580b0bfc450be6aeac8369c37aab
Brett Logan <lindluni@github.com>
2021-02-11 16:53:01 -0500

    Add Service Labels and Cleanup Using Standard Tools
    Add a service label to all containers launch by compose.
    This allows us to filter on the label for removing running
    containers.
    
    Use filtering for querying running containers and
    images to make sure we only target the containers
    and images we want to remove.
    
    Signed-off-by: Brett Logan <lindluni@github.com>

--- test-network/network.sh
+++ test-network/network.sh
@@ -19,27 +19,19 @@ export VERBOSE=false
 . scripts/utils.sh
 
 # Obtain CONTAINER_IDS and remove them
-# TODO Might want to make this optional - could clear other containers
 # This function is called when you bring a network down
 function clearContainers() {
-  CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer.*/) {print $1}')
-  if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
-    infoln "No containers available for deletion"
-  else
-    docker rm -f $CONTAINER_IDS
-  fi
+  infoln "Removing remaining containers"
+  docker rm -f $(docker ps -aq --filter label=service=hyperledger-fabric) 2>/dev/null || true
+  docker rm -f $(docker ps -aq --filter name='dev-peer*') 2>/dev/null || true
 }
 
 # Delete any images that were generated as a part of this setup
 # specifically the following images are often left behind:
 # This function is called when you bring the network down
 function removeUnwantedImages() {
-  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*/) {print $3}')
-  if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
-    infoln "No images available for deletion"
-  else
-    docker rmi -f $DOCKER_IMAGE_IDS
-  fi
+  infoln "Removing generated chaincode docker images"
+  docker image rm -f $(docker images -aq --filter reference='dev-peer*') 2>/dev/null || true
 }
 
 # Versions of fabric known not to work with the test network
