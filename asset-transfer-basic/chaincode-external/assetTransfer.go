7ee1bf793d1bffd77b3c929aaf37847c5055ab90
Arun S M <aruna.mohan@walmart.com>
2021-04-20 14:36:46 +0530

    TLS enabled external chaincode example (#438)
    * Enable TLS on asset-transfer-basic external chaincode
    
    1. Introduce environment variables to enable or disable TLS
    at runtime. Also, introduce environment variables which carries
    the server key, cert and the client machine's root CA cert
    information.
    2. Read the environment variables to decide with appropriate
    assumptions on default values to work as is today.
    
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>
    
    * Update the chaincode.env with new options
    
    1. Introduce four new environment variables to the default
    chaincode environment file.
    2. Add two new environment files for easy/automation running
    of the external chaincodes for different organizations.
    
    Note that when TLS enabled, the package contents will change.
    So each organization will have their own package ID
    generated unlike earlier. Also, there will be two chaincode
    containers running instead of one for the given example.
    
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>
    
    * Add docker compose file for running chaincodes
    
    The file will build and run the chaincode on the fly.
    Also, it will make use of the available files which is
    configured by the user as part of execution. This will
    help in bringup/bringdown of the chaincode containers.
    
    In addition, it will help in updating the environment
    variables on the go.
    
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>
    
    * Introduce crypto directory and ignore the pem contents
    
    The directory is used by the docker-compose file for
    automation. Instructions in the README.md file will
    suffice the filling up of required files in this directory.
    
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>
    
    * Instructions on TLS enabled external chaincode server
    
    1. Add instructions to generate a chaincode server TLS
    parameters.
    2. Add instructions on different parameters that peer node
    expects and the chaincode expects.
    3. Add instructions on steps to be followed in asset-transfer
    -basic to work with iTLS enabled exteral chaincode.
    
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>
    
    * Enable chaincode to work without TLS as well
    
    1. Root certificate is an optional parameter supplied for
    chaincode to verify the peer's connection in response.
    2. If TLS is enabled, then specify the key and cert
    parameters. Do not expect it otherwise.
    
    Signed-off-by: S m, Aruna <arun.s.m.cse@gmail.com>

--- asset-transfer-basic/chaincode-external/assetTransfer.go
+++ asset-transfer-basic/chaincode-external/assetTransfer.go
@@ -7,8 +7,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
@@ -224,12 +226,66 @@ func main() {
 		CCID:    config.CCID,
 		Address: config.Address,
 		CC:      chaincode,
-		TLSProps: shim.TLSProperties{
-			Disabled: true,
-		},
+		TLSProps: getTLSProperties(),
 	}
 
 	if err := server.Start(); err != nil {
 		log.Panicf("error starting asset-transfer-basic chaincode: %s", err)
 	}
+}
+
+func getTLSProperties() shim.TLSProperties {
+	// Check if chaincode is TLS enabled
+	tlsDisabledStr := getEnvOrDefault("CHAINCODE_TLS_DISABLED", "true")
+	key := getEnvOrDefault("CHAINCODE_TLS_KEY", "")
+	cert := getEnvOrDefault("CHAINCODE_TLS_CERT", "")
+	clientCACert := getEnvOrDefault("CHAINCODE_CLIENT_CA_CERT", "")
+
+	// convert tlsDisabledStr to boolean
+	tlsDisabled := getBoolOrDefault(tlsDisabledStr, false)
+	var keyBytes, certBytes, clientCACertBytes []byte
+	var err error
+
+	if !tlsDisabled {
+		keyBytes, err = ioutil.ReadFile(key)
+		if err != nil {
+			log.Panicf("error while reading the crypto file: %s", err)
+		}
+		certBytes, err = ioutil.ReadFile(cert)
+		if err != nil {
+			log.Panicf("error while reading the crypto file: %s", err)
+		}
+	}
+	// Did not request for the peer cert verification
+	if clientCACert != "" {
+		clientCACertBytes, err = ioutil.ReadFile(clientCACert)
+		if err != nil {
+			log.Panicf("error while reading the crypto file: %s", err)
+		}
+	}
+
+	return shim.TLSProperties{
+		Disabled: tlsDisabled,
+		Key: keyBytes,
+		Cert: certBytes,
+		ClientCACerts: clientCACertBytes,
+	}
+}
+
+func getEnvOrDefault(env, defaultVal string) string {
+	value, ok := os.LookupEnv(env)
+	if !ok {
+		value = defaultVal
+	}
+	return value
+}
+
+// Note that the method returns default value if the string
+// cannot be parsed!
+func getBoolOrDefault(value string, defaultVal bool) bool {
+	parsed, err := strconv.ParseBool(value)
+	if err!= nil {
+		return defaultVal
+	}
+	return parsed
 }
