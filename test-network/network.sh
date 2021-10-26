89c7739482db46a184e256c57fb1b6470a909cce
Justin Yang <justin.yang@themedium.io>
2021-10-26 18:51:43 +0900

    test-network: Make the regexp of checking version more strict (backport #515)
    Signed-off-by: Justin Yang <justin.yang@themedium.io>

--- test-network/network.sh
+++ test-network/network.sh
@@ -61,8 +61,8 @@ function checkPrereqs() {
   fi
   # use the fabric tools container to see if the samples and binaries match your
   # docker images
-  LOCAL_VERSION=$(peer version | sed -ne 's/ Version: //p')
-  DOCKER_IMAGE_VERSION=$(docker run --rm hyperledger/fabric-tools:$IMAGETAG peer version | sed -ne 's/ Version: //p' | head -1)
+  LOCAL_VERSION=$(peer version | sed -ne 's/^ Version: //p')
+  DOCKER_IMAGE_VERSION=$(docker run --rm hyperledger/fabric-tools:$IMAGETAG peer version | sed -ne 's/^ Version: //p')
 
   infoln "LOCAL_VERSION=$LOCAL_VERSION"
   infoln "DOCKER_IMAGE_VERSION=$DOCKER_IMAGE_VERSION"
