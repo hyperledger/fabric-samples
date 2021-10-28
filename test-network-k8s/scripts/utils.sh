6627cd6d6544f53fff1171f6be7d58a0686395ee
Matthew B White <whitemat@uk.ibm.com>
2021-10-28 15:07:42 +0100

    review cmts
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- test-network-k8s/scripts/utils.sh
+++ test-network-k8s/scripts/utils.sh
@@ -19,8 +19,7 @@ function logging_init() {
   exec 1>>${DEBUG_FILE} 2>>${DEBUG_FILE}
 
   # There can be a race between the tail starting and the next log statement
-  # write nothing to the file to kill a (very small) bit of time
-  echo -ne >>${LOG_FILE}
+  sleep 0.5
 }
 
 function exit_fn() {
