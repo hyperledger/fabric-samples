e9cfb785d316f39ce678dfb33269874aa4e3e362
David Enyeart <enyeart@us.ibm.com>
2022-02-01 03:13:37 -0500

    Update test-network to use org level ca-cert.pem
    Although test network scripts functionally worked, they demonstrated
    some strange patterns around use of the ca cert.
    
    This change makes it clear that the ca cert is configured and used
    at the org level, and not related to any individual peer or orderer node.
    Also that when connecting to individual servers, clients can pass the
    org level CA cert as the root of trust when establishing TLS connections.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- test-network/CHAINCODE_AS_A_SERVICE_TUTORIAL.md
+++ test-network/CHAINCODE_AS_A_SERVICE_TUTORIAL.md
@@ -45,7 +45,7 @@ Note that the order listed isn't mandatory. The key thing is that the containers
 This sequence can be run as follows
 
 ```bash
-./network.sh deployCCAAS  -ccn basicts -ccp ../asset-transfer-basic/chaincode-typescript 
+./network.sh deployCCAAS  -ccn basicts -ccp ../asset-transfer-basic/chaincode-typescript
 ```
 
 This is very similar to the `deployCC` command, it needs the name, and path. But also needs to have the port the chaincode container is going use. As each container is on the `fabric-test` network, you might wish to alter this so there are no collisions with other chaincode containers.
@@ -59,7 +59,7 @@ To test things are working you can invoke the 'Contract Metadata' function. For 
 
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
 export FABRIC_CFG_PATH=${PWD}/../config
--- test-network/scripts/envVar.sh
+++ test-network/scripts/envVar.sh
@@ -11,10 +11,10 @@
 . scripts/utils.sh
 
 export CORE_PEER_TLS_ENABLED=true
-export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-export PEER0_ORG3_CA=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
+export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
+export PEER0_ORG3_CA=${PWD}/organizations/peerOrganizations/org3.example.com/tlsca/tlsca.org3.example.com-cert.pem
 export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
 export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
 
@@ -52,7 +52,7 @@ setGlobals() {
   fi
 }
 
-# Set environment variables for use in the CLI container 
+# Set environment variables for use in the CLI container
 setGlobalsCLI() {
   setGlobals $1
 
