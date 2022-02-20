ad8254179ae29e4752608d82ddff26642288b8ce
Sam Yuan <yy19902439@126.com>
2022-02-20 13:31:18 +0800

    fix up according to pr review comments
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- test-network-k8s/scripts/test_network.sh
+++ test-network-k8s/scripts/test_network.sh
@@ -209,7 +209,6 @@ function network_up() {
 
   # Test Network
   create_local_MSP
-  create_couchdb
 
   launch_orderers
   launch_peers
@@ -253,13 +252,4 @@ function scrub_org_volumes() {
 function network_down() {
   stop_services
   scrub_org_volumes
-}
-
-function create_couchdb() {
-  push_fn "Launching couchdb"
-  launch ./kube/org1/org1-peer1-couchdb.yaml
-  launch ./kube/org1/org1-peer2-couchdb.yaml
-  launch ./kube/org2/org2-peer1-couchdb.yaml
-  launch ./kube/org2/org2-peer2-couchdb.yaml
-  pop_fn
 }
