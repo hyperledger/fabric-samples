c712d9333d65c6971e7236a17c1f51f3ed9c0a6e
renjithkn@gmail.com <renjithkn@gmail.com>
2022-03-05 14:06:01 +0000

    Signed-off-by: Renjith K N <renjithkn@gmail.com>.
    ERC 721 implementation of java chaincode.
    
    Signed-off-by: renjithkn@gmail.com <renjithkn@gmail.com>
    Signed-off-by: renjithpta <renjithkn@gmail.com>

---
+++ token-erc-721/chaincode-java/src/main/java/org/hyperledger/fabric/samples/erc721/models/Approval.java
@@ -0,0 +1,131 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.fabric.samples.erc721.models;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+import org.json.JSONObject;
+
+import com.owlike.genson.Genson;
+import com.owlike.genson.annotation.JsonProperty;
+
+@DataType()
+public final class Approval {
+
+  @Property()
+  @JsonProperty("owner")
+  private String owner;
+
+  @Property()
+  @JsonProperty("operator")
+  private String operator;
+
+  @Property()
+  @JsonProperty("approved")
+  private boolean approved;
+  
+  
+ /**
+  * Default constructor
+  */
+  public Approval() {
+    super();
+
+  }
+  /**
+   * Constructor of the class 
+   * @param owner
+   * @param operator
+   * @param approved
+   */
+  public Approval(@JsonProperty("owner") final String owner,
+      @JsonProperty("operator") final String operator,
+      @JsonProperty("approved") final boolean approved) {
+    super();
+    this.owner = owner;
+    this.operator = operator;
+    this.approved = approved;
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public String getOwner() {
+    return owner;
+  }
+
+  public void setOwner(final String owner) {
+    this.owner = owner;
+  }
+
+  /**
+   * 
+   * @return
+   */
+
+  public String getOperator() {
+    return operator;
+  }
+
+  /**
+   * 
+   * @param operator
+   */
+  public void setOperator(final String operator) {
+    this.operator = operator;
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public boolean isApproved() {
+    return approved;
+  }
+
+  /**
+   * 
+   * @param approved
+   */
+  public void setApproved(final boolean approved) {
+    this.approved = approved;
+  }
+
+  /**
+   * 
+   * @return JSON String of the Approval object.
+   */
+  public String toJSONString() {
+
+    return new Genson().serialize(this).toString();
+  }
+
+  /**
+   * 
+   * @param data
+   * @return
+   */
+  public static Approval fromJSONString(final String data) {
+    JSONObject json = new JSONObject(data);
+    Approval approver = new Approval(json.getString("owner"), json.getString("operator"),
+        new Boolean(json.getBoolean("approved")));
+
+    return approver;
+  }
+
+  /**
+   * Constructs new Approval from JSON String.
+   * 
+   * @param json Approval format.
+   * @return
+   */
+  public static Approval fromBytes(final byte[] bytes) {
+    return new Genson().deserialize(new String(bytes, UTF_8), Approval.class);
+
+  }
+
+}
