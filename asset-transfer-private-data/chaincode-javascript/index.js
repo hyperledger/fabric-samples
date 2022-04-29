304e84ec9692a624756da69aae2b96a30a7df7ab
abdou.chakhkhar <abdelmoula.chakhkhar@uit.ac.ma>
2022-04-29 15:25:02 +0000

    add js cc for the private data
    Signed-off-by: abdou.chakhkhar <abdelmoula.chakhkhar@uit.ac.ma>

---
+++ asset-transfer-private-data/chaincode-javascript/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const PrivateAssetTransfer = require('./lib/PrivateAssetTransfer');
+
+module.exports.PrivateAssetTransfer = PrivateAssetTransfer;
+module.exports.contracts = [PrivateAssetTransfer];
