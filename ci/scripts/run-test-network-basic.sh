02465a4386c07faf70f73e539600bc553ff63e44
sapthasurendran <saptha.surendran@ibm.com>
2022-07-01 12:20:43 +0530

    test file directory
    Signed-off-by: sapthasurendran <saptha.surendran@ibm.com>

--- ci/scripts/run-test-network-basic.sh
+++ ci/scripts/run-test-network-basic.sh
@@ -26,66 +26,66 @@ function stopNetwork() {
   ./network.sh down
 }
 
-# Run Go application
-createNetwork
-print "Initializing Go application"
-pushd ../asset-transfer-basic/application-go
-print "Executing AssetTransfer.go"
-go run .
-popd
-stopNetwork
+# # Run Go application
+# createNetwork
+# print "Initializing Go application"
+# pushd ../asset-transfer-basic/application-go
+# print "Executing AssetTransfer.go"
+# go run .
+# popd
+# stopNetwork
 
-# Run Java application
-createNetwork
-print "Initializing Java application"
-pushd ../asset-transfer-basic/application-java
-print "Executing Gradle Run"
-gradle run
-popd
-stopNetwork
+# # Run Java application
+# createNetwork
+# print "Initializing Java application"
+# pushd ../asset-transfer-basic/application-java
+# print "Executing Gradle Run"
+# gradle run
+# popd
+# stopNetwork
 
-# Run Java application using gateway
-createNetwork
-print "Initializing Java application"
-pushd ../asset-transfer-basic/application-gateway-java
-print "Executing Gradle Run"
-./gradlew run
-popd
-stopNetwork
+# # Run Java application using gateway
+# createNetwork
+# print "Initializing Java application"
+# pushd ../asset-transfer-basic/application-gateway-java
+# print "Executing Gradle Run"
+# ./gradlew run
+# popd
+# stopNetwork
 
-# Run Javascript application
-createNetwork
-print "Initializing Javascript application"
-pushd ../asset-transfer-basic/application-javascript
-npm install
-print "Executing app.js"
-node app.js
-popd
-stopNetwork
+# # Run Javascript application
+# createNetwork
+# print "Initializing Javascript application"
+# pushd ../asset-transfer-basic/application-javascript
+# npm install
+# print "Executing app.js"
+# node app.js
+# popd
+# stopNetwork
 
-# Run typescript application
-createNetwork
-print "Initializing Typescript application"
-pushd ../asset-transfer-basic/application-typescript
-npm install
-print "Building app.ts"
-npm run build
-print "Running the output app"
-node dist/app.js
-popd
-stopNetwork
+# # Run typescript application
+# createNetwork
+# print "Initializing Typescript application"
+# pushd ../asset-transfer-basic/application-typescript
+# npm install
+# print "Building app.ts"
+# npm run build
+# print "Running the output app"
+# node dist/app.js
+# popd
+# stopNetwork
 
-# Run gateway typescript application
-createNetwork
-print "Initializing Typescript gateway application"
-pushd ../asset-transfer-basic/application-gateway-typescript
-npm install
-print "Building app.ts"
-npm run build
-print "Running the output app"
-node dist/app.js
-popd
-stopNetwork
+# # Run gateway typescript application
+# createNetwork
+# print "Initializing Typescript gateway application"
+# pushd ../asset-transfer-basic/application-gateway-typescript
+# npm install
+# print "Building app.ts"
+# npm run build
+# print "Running the output app"
+# node dist/app.js
+# popd
+# stopNetwork
 
 # Run typescript HSM application
 createNetwork
@@ -107,6 +107,11 @@ echo 'Delete fabric-ca-client from samples bin'
 rm ../bin/fabric-ca-client
 echo 'go install pkcs11 enabled fabric-ca-client'
 go install -tags pkcs11 github.com/hyperledger/fabric-ca/cmd/fabric-ca-client@latest
+FILE=../bin/fabric-ca-client
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+ls -la ../bin
 createNetwork
 print "Initializing Typescript HSM gateway application"
 pushd ../asset-transfer-basic/application-gateway-hsm/scripts/
@@ -136,34 +141,34 @@ popd
 popd
 stopNetwork
 
-# Run Go gateway application
-createNetwork
-print "Initializing Go gateway application"
-pushd ../asset-transfer-basic/application-gateway-go
-print "Executing AssetTransfer.go"
-go run .
-popd
-stopNetwork
+# # Run Go gateway application
+# createNetwork
+# print "Initializing Go gateway application"
+# pushd ../asset-transfer-basic/application-gateway-go
+# print "Executing AssetTransfer.go"
+# go run .
+# popd
+# stopNetwork
 
-# Run off-chain data TypeScript application
-createNetwork
-print "Initializing Typescript off-chain data application"
-pushd ../off_chain_data/application-typescript
-rm -f checkpoint.json store.log
-npm install
-print "Running the output app"
-SIMULATED_FAILURE_COUNT=1 npm start getAllAssets transact getAllAssets listen
-SIMULATED_FAILURE_COUNT=1 npm start listen
-popd
-stopNetwork
+# # Run off-chain data TypeScript application
+# createNetwork
+# print "Initializing Typescript off-chain data application"
+# pushd ../off_chain_data/application-typescript
+# rm -f checkpoint.json store.log
+# npm install
+# print "Running the output app"
+# SIMULATED_FAILURE_COUNT=1 npm start getAllAssets transact getAllAssets listen
+# SIMULATED_FAILURE_COUNT=1 npm start listen
+# popd
+# stopNetwork
 
-# Run off-chain data Java application
-createNetwork
-print "Initializing Typescript off-chain data application"
-pushd ../off_chain_data/application-java
-rm -f app/checkpoint.json app/store.log
-print "Running the output app"
-SIMULATED_FAILURE_COUNT=1 ./gradlew run --quiet --args='getAllAssets transact getAllAssets listen'
-SIMULATED_FAILURE_COUNT=1 ./gradlew run --quiet --args=listen
-popd
-stopNetwork
+# # Run off-chain data Java application
+# createNetwork
+# print "Initializing Typescript off-chain data application"
+# pushd ../off_chain_data/application-java
+# rm -f app/checkpoint.json app/store.log
+# print "Running the output app"
+# SIMULATED_FAILURE_COUNT=1 ./gradlew run --quiet --args='getAllAssets transact getAllAssets listen'
+# SIMULATED_FAILURE_COUNT=1 ./gradlew run --quiet --args=listen
+# popd
+# stopNetwork
