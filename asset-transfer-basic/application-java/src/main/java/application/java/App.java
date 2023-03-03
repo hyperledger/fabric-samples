b4f4ad914b59bacc942f2973193a8b8099e048a7
David Enyeart <enyeart@us.ibm.com>
2023-03-03 00:15:29 -0500

    Return exit(1) if application-java fails
    If application-java fails, return an exit(1) code,
    so that callers such as Github Actions CI can detect the failure.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- asset-transfer-basic/application-java/src/main/java/application/java/App.java
+++ asset-transfer-basic/application-java/src/main/java/application/java/App.java
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// Running TestApp: 
-// gradle runApp 
+// Running TestApp:
+// gradle runApp
 
 package application.java;
 
@@ -110,6 +110,7 @@ public class App {
 		}
 		catch(Exception e){
 			System.err.println(e);
+			System.exit(1);
 		}
 
 	}
