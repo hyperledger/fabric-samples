a5164eeabd16ca095b55116f2131e8f8cfbeb0af
thejjw <72130076+thejjw@users.noreply.github.com>
2024-01-15 10:28:44 +0900

    Fix certPath in assetTransfer.go
    current version of network.sh does not generate cert.pem as filename
    
    Signed-off-by: thejjw <72130076+thejjw@users.noreply.github.com>

--- asset-transfer-basic/application-gateway-go/assetTransfer.go
+++ asset-transfer-basic/application-gateway-go/assetTransfer.go
@@ -28,7 +28,7 @@ import (
 const (
 	mspID        = "Org1MSP"
 	cryptoPath   = "../../test-network/organizations/peerOrganizations/org1.example.com"
-	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
+	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem"
 	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
 	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
 	peerEndpoint = "localhost:7051"
