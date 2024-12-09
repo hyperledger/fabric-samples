90f1ff845b072070c8df64cfba4852fb7c9b2504
Stanislav Jakuschevskij <stas@two-giants.com>
2024-12-09 19:13:27 +0100

    Implement block processor
    Started implementing block processor struct and the process method.
    Implemented getting valid transactions from the last processed index.
    Added data structures needed for the store.
    
    Decomposed the parser.Block.Transactions() method into readable chunks.

--- off_chain_data/application-go/listen.go
+++ off_chain_data/application-go/listen.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"offChainData/parser"
 	"strconv"
 
 	"github.com/hyperledger/fabric-gateway/pkg/client"
@@ -35,6 +36,7 @@ func listen(clientConnection *grpc.ClientConn) {
 		fmt.Printf("Simulating a write failure every %d transactions", simulatedFailureCount)
 	}
 
+	// TODO put into infinite loop like in public docs example
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -48,8 +50,107 @@ func listen(clientConnection *grpc.ClientConn) {
 	}
 
 	for blockProto := range blocks {
-		checkpointer.CheckpointBlock(blockProto.GetHeader().GetNumber())
+		blockPr := blockProcessor{
+			parser.ParseBlock(blockProto),
+			checkpointer,
+			func(data ledgerUpdate) {},
+		}
+		blockPr.process()
 	}
+}
+
+type store = func(data ledgerUpdate)
+
+type ledgerUpdate struct {
+	blockNumber   uint64
+	transactionId string
+	writes        []write
+}
+
+type write struct {
+	channelName string
+	namespace   string
+	key         string
+	isDelete    bool
+	value       []byte
+}
+
+type blockProcessor struct {
+	block        *parser.Block
+	checkpointer *client.FileCheckpointer
+	_store       store
+}
+
+func (b *blockProcessor) process() {
+	blockNumber := b.block.Number()
+
+	fmt.Println("Received block", blockNumber)
+
+	for _, transaction := range b.validTransactions() {
+		fmt.Println("CONTINUE HERE:", transaction.ChannelHeader().GetTxId())
+	}
+}
+
+func (b blockProcessor) validTransactions() []*parser.Transaction {
+	result := []*parser.Transaction{}
+	for _, transaction := range b.getNewTransactions() {
+		if transaction.IsValid() {
+			result = append(result, transaction)
+		}
+	}
+	return result
+}
+
+func (b *blockProcessor) getNewTransactions() []*parser.Transaction {
+	transactions := b.block.Transactions()
+
+	lastTransactionId := b.checkpointer.TransactionID()
+	if lastTransactionId == "" {
+		// No previously processed transactions within this block so all are new
+		return transactions
+	}
+
+	// Ignore transactions up to the last processed transaction ID
+	lastProcessedIndex := b.findLastProcessedIndex()
+	return transactions[lastProcessedIndex+1:]
+}
+
+func (b blockProcessor) findLastProcessedIndex() int {
+	blockTransactionIds := []string{}
+	for _, transaction := range b.block.Transactions() {
+		blockTransactionIds = append(blockTransactionIds, transaction.ChannelHeader().GetTxId())
+	}
+
+	lastTransactionId := b.checkpointer.TransactionID()
+	lastProcessedIndex := -1
+	for index, id := range blockTransactionIds {
+		if id == lastTransactionId {
+			lastProcessedIndex = index
+		}
+	}
+	if lastProcessedIndex < 0 {
+		panic(
+			fmt.Errorf(
+				"checkpoint transaction ID %s not found in block %d containing transactions: %s",
+				lastTransactionId,
+				b.block.Number(),
+				joinByComma(blockTransactionIds),
+			),
+		)
+	}
+	return lastProcessedIndex
+}
+
+func joinByComma(list []string) string {
+	result := ""
+	for index, item := range list {
+		if len(list)-1 == index {
+			result += item
+		} else {
+			result += item + ", "
+		}
+	}
+	return result
 }
 
 func getSimulatedFailureCount() uint {
