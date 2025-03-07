5be225cf5a474efeb0204a6c9577f49d11c5907e
XZSt4nce <xzstnc@mail.ru>
2025-03-08 02:26:02 +0300

    JSDoc returns Promises in async functions; Using DRY for ClientAccountID, BalanceOf etc; Using ClientAccountMSPID; Using CheckAuthorization; Using _update for minting, burning and transfering tokens; Additional functions for approving, spending allowance and checking empty values; Updated tests
    Signed-off-by: XZSt4nce <xzstnc@mail.ru>

--- token-erc-20/chaincode-javascript/test/tokenERC20.test.js
+++ token-erc-20/chaincode-javascript/test/tokenERC20.test.js
@@ -20,15 +20,13 @@ chai.should();
 chai.use(chaiAsPromised);
 
 describe('Chaincode', () => {
-    let sandbox;
     let token;
     let ctx;
     let mockStub;
     let mockClientIdentity;
 
-    beforeEach('Sandbox creation', async () => {
-        sandbox = sinon.createSandbox();
-        token = new TokenERC20Contract('token-erc20');
+    beforeEach(async () => {
+        token = new TokenERC20Contract();
 
         ctx = sinon.createStubInstance(Context);
         mockStub = sinon.createStubInstance(ChaincodeStub);
@@ -36,41 +34,34 @@ describe('Chaincode', () => {
         mockClientIdentity = sinon.createStubInstance(ClientIdentity);
         ctx.clientIdentity = mockClientIdentity;
 
+        mockClientIdentity.getMSPID.returns('Org1MSP');
+
         await token.Initialize(ctx, 'some name', 'some symbol', '2');
-
-        mockStub.putState.resolves('some state');
-        mockStub.setEvent.returns('set event');
-
-    });
-
-    afterEach('Sandbox restoration', () => {
-        sandbox.restore();
+        mockStub.getState.withArgs('name').resolves(Buffer.from('some name'));
+        mockStub.getState.withArgs('symbol').resolves(Buffer.from('some symbol'));
+        mockStub.getState.withArgs('decimals').resolves(Buffer.from('2'));
+        mockStub.getState.withArgs('totalSupply').resolves(Buffer.from('0'));
+        console.log('Initialized');
     });
 
     describe('#TokenName', () => {
         it('should work', async () => {
-            mockStub.getState.resolves('some state');
-
             const response = await token.TokenName(ctx);
             sinon.assert.calledWith(mockStub.getState, 'name');
-            expect(response).to.equals('some state');
+            expect(response).to.equals('some name');
         });
     });
 
     describe('#Symbol', () => {
         it('should work', async () => {
-            mockStub.getState.resolves('some state');
-
             const response = await token.Symbol(ctx);
             sinon.assert.calledWith(mockStub.getState, 'symbol');
-            expect(response).to.equals('some state');
+            expect(response).to.equals('some symbol');
         });
     });
 
     describe('#Decimals', () => {
         it('should work', async () => {
-            mockStub.getState.resolves(Buffer.from('2'));
-
             const response = await token.Decimals(ctx);
             sinon.assert.calledWith(mockStub.getState, 'decimals');
             expect(response).to.equals(2);
@@ -79,11 +70,9 @@ describe('Chaincode', () => {
 
     describe('#TotalSupply', () => {
         it('should work', async () => {
-            mockStub.getState.resolves(Buffer.from('10000'));
-
             const response = await token.TotalSupply(ctx);
             sinon.assert.calledWith(mockStub.getState, 'totalSupply');
-            expect(response).to.equals(10000);
+            expect(response).to.equals(0);
         });
     });
 
@@ -100,16 +89,16 @@ describe('Chaincode', () => {
     describe('#_transfer', () => {
 
         it('should fail when the sender and the receipient are the same', async () => {
-            await expect(token._transfer(ctx, 'Alice', 'Alice', '1000'))
-                .to.be.rejectedWith(Error, 'cannot transfer to and from same client account');
+            const response = token._transfer(ctx, 'Alice', 'Alice', '1000');
+            await expect(response).to.be.rejectedWith(Error, 'cannot transfer to and from same client account');
         });
 
         it('should fail when the sender does not have enough token', async () => {
             mockStub.createCompositeKey.withArgs('balance', ['Alice']).returns('balance_Alice');
             mockStub.getState.withArgs('balance_Alice').resolves(Buffer.from('500'));
 
-            await expect(token._transfer(ctx, 'Alice', 'Bob', '1000'))
-                .to.be.rejectedWith(Error, 'client account Alice has insufficient funds.');
+            const response = token._transfer(ctx, 'Alice', 'Bob', '1000');
+            await expect(response).to.be.rejectedWith(Error, 'client account Alice has insufficient funds.');
         });
 
         it('should transfer to a new account when the sender has enough token', async () => {
@@ -119,10 +108,9 @@ describe('Chaincode', () => {
             mockStub.createCompositeKey.withArgs('balance', ['Bob']).returns('balance_Bob');
             mockStub.getState.withArgs('balance_Bob').resolves(null);
 
-            const response = await token._transfer(ctx, 'Alice', 'Bob', '1000');
-            sinon.assert.calledWith(mockStub.putState.getCall(0), 'balance_Alice', Buffer.from('0'));
-            sinon.assert.calledWith(mockStub.putState.getCall(1), 'balance_Bob', Buffer.from('1000'));
-            expect(response).to.equals(true);
+            await token._transfer(ctx, 'Alice', 'Bob', '1000');
+            sinon.assert.calledWith(mockStub.putState.getCall(4), 'balance_Alice', Buffer.from('0'));
+            sinon.assert.calledWith(mockStub.putState.getCall(5), 'balance_Bob', Buffer.from('1000'));
         });
 
         it('should transfer to the existing account when the sender has enough token', async () => {
@@ -132,10 +120,9 @@ describe('Chaincode', () => {
             mockStub.createCompositeKey.withArgs('balance', ['Bob']).returns('balance_Bob');
             mockStub.getState.withArgs('balance_Bob').resolves(Buffer.from('2000'));
 
-            const response = await token._transfer(ctx, 'Alice', 'Bob', '1000');
-            sinon.assert.calledWith(mockStub.putState.getCall(0), 'balance_Alice', Buffer.from('0'));
-            sinon.assert.calledWith(mockStub.putState.getCall(1), 'balance_Bob', Buffer.from('3000'));
-            expect(response).to.equals(true);
+            await token._transfer(ctx, 'Alice', 'Bob', '1000');
+            sinon.assert.calledWith(mockStub.putState.getCall(4), 'balance_Alice', Buffer.from('0'));
+            sinon.assert.calledWith(mockStub.putState.getCall(5), 'balance_Bob', Buffer.from('3000'));
         });
 
     });
@@ -143,7 +130,9 @@ describe('Chaincode', () => {
     describe('#Transfer', () => {
         it('should work', async () => {
             mockClientIdentity.getID.returns('Alice');
-            sinon.stub(token, '_transfer').returns(true);
+
+            mockStub.createCompositeKey.withArgs('balance', ['Alice']).returns('balance_Alice');
+            mockStub.getState.withArgs('balance_Alice').resolves(Buffer.from('1000'));
 
             const response = await token.Transfer(ctx, 'Bob', '1000');
             const event = { from: 'Alice', to: 'Bob', value: 1000 };
@@ -159,17 +148,18 @@ describe('Chaincode', () => {
             mockStub.createCompositeKey.withArgs('allowance', ['Alice', 'Charlie']).returns('allowance_Alice_Charlie');
             mockStub.getState.withArgs('allowance_Alice_Charlie').resolves(Buffer.from('0'));
 
-            await expect(token.TransferFrom(ctx, 'Alice', 'Bob', '1000'))
-                .to.be.rejectedWith(Error, 'The spender does not have enough allowance to spend.');
+            const response = token.TransferFrom(ctx, 'Alice', 'Bob', '1000');
+            await expect(response).to.be.rejectedWith(Error, 'The spender does not have enough allowance to spend.');
         });
 
         it('should transfer when the spender is allowed to spend the token', async () => {
             mockClientIdentity.getID.returns('Charlie');
 
+            mockStub.createCompositeKey.withArgs('balance', ['Alice']).returns('balance_Alice');
+            mockStub.getState.withArgs('balance_Alice').resolves(Buffer.from('1000'));
+
             mockStub.createCompositeKey.withArgs('allowance', ['Alice', 'Charlie']).returns('allowance_Alice_Charlie');
             mockStub.getState.withArgs('allowance_Alice_Charlie').resolves(Buffer.from('3000'));
-
-            sinon.stub(token, '_transfer').returns(true);
 
             const response = await token.TransferFrom(ctx, 'Alice', 'Bob', '1000');
             sinon.assert.calledWith(mockStub.putState, 'allowance_Alice_Charlie', Buffer.from('2000'));
@@ -210,48 +200,43 @@ describe('Chaincode', () => {
 
         it('should failed if called a second time', async () => {
             // We consider it has already been initialized in the before-each statement
-            await expect(await token.Initialize(ctx, 'some name', 'some symbol', '2'))
-                .to.be.rejectedWith(Error, 'contract options are already set, client is not authorized to change them');
+            const response = token.Initialize(ctx, 'some name', 'some symbol', '2');
+            await expect(response).to.be.rejectedWith(Error, 'contract options are already set, client is not authorized to change them');
         });
     });
 
     describe('#Mint', () => {
         it('should add token to a new account and a new total supply', async () => {
-            mockClientIdentity.getMSPID.returns('Org1MSP');
             mockClientIdentity.getID.returns('Alice');
             mockStub.createCompositeKey.returns('balance_Alice');
-            mockStub.getState.withArgs('balance_Alice').resolves(null);
-            mockStub.getState.withArgs('totalSupply').resolves(null);
 
             const response = await token.Mint(ctx, '1000');
-            sinon.assert.calledWith(mockStub.putState.getCall(0), 'balance_Alice', Buffer.from('1000'));
-            sinon.assert.calledWith(mockStub.putState.getCall(1), 'totalSupply', Buffer.from('1000'));
+            sinon.assert.calledWith(mockStub.putState.getCall(4), 'balance_Alice', Buffer.from('1000'));
+            sinon.assert.calledWith(mockStub.putState.getCall(5), 'totalSupply', Buffer.from('1000'));
             expect(response).to.equals(true);
         });
 
         it('should add token to the existing account and the existing total supply', async () => {
-            mockClientIdentity.getMSPID.returns('Org1MSP');
             mockClientIdentity.getID.returns('Alice');
             mockStub.createCompositeKey.returns('balance_Alice');
             mockStub.getState.withArgs('balance_Alice').resolves(Buffer.from('1000'));
             mockStub.getState.withArgs('totalSupply').resolves(Buffer.from('2000'));
 
             const response = await token.Mint(ctx, '1000');
-            sinon.assert.calledWith(mockStub.putState.getCall(0), 'balance_Alice', Buffer.from('2000'));
-            sinon.assert.calledWith(mockStub.putState.getCall(1), 'totalSupply', Buffer.from('3000'));
+            sinon.assert.calledWith(mockStub.putState.getCall(4), 'balance_Alice', Buffer.from('2000'));
+            sinon.assert.calledWith(mockStub.putState.getCall(5), 'totalSupply', Buffer.from('3000'));
             expect(response).to.equals(true);
         });
 
         it('should add token to a new account and the existing total supply', async () => {
-            mockClientIdentity.getMSPID.returns('Org1MSP');
             mockClientIdentity.getID.returns('Alice');
             mockStub.createCompositeKey.returns('balance_Alice');
             mockStub.getState.withArgs('balance_Alice').resolves(null);
             mockStub.getState.withArgs('totalSupply').resolves(Buffer.from('2000'));
 
             const response = await token.Mint(ctx, '1000');
-            sinon.assert.calledWith(mockStub.putState.getCall(0), 'balance_Alice', Buffer.from('1000'));
-            sinon.assert.calledWith(mockStub.putState.getCall(1), 'totalSupply', Buffer.from('3000'));
+            sinon.assert.calledWith(mockStub.putState.getCall(4), 'balance_Alice', Buffer.from('1000'));
+            sinon.assert.calledWith(mockStub.putState.getCall(5), 'totalSupply', Buffer.from('3000'));
             expect(response).to.equals(true);
         });
 
@@ -259,15 +244,14 @@ describe('Chaincode', () => {
 
     describe('#Burn', () => {
         it('should work', async () => {
-            mockClientIdentity.getMSPID.returns('Org1MSP');
             mockClientIdentity.getID.returns('Alice');
             mockStub.createCompositeKey.returns('balance_Alice');
             mockStub.getState.withArgs('balance_Alice').resolves(Buffer.from('1000'));
             mockStub.getState.withArgs('totalSupply').resolves(Buffer.from('2000'));
 
             const response = await token.Burn(ctx, '1000');
-            sinon.assert.calledWith(mockStub.putState.getCall(0), 'balance_Alice', Buffer.from('0'));
-            sinon.assert.calledWith(mockStub.putState.getCall(1), 'totalSupply', Buffer.from('1000'));
+            sinon.assert.calledWith(mockStub.putState.getCall(4), 'balance_Alice', Buffer.from('0'));
+            sinon.assert.calledWith(mockStub.putState.getCall(5), 'totalSupply', Buffer.from('1000'));
             expect(response).to.equals(true);
         });
     });
@@ -278,7 +262,7 @@ describe('Chaincode', () => {
             mockStub.createCompositeKey.returns('balance_Alice');
             mockStub.getState.resolves(Buffer.from('1000'));
 
-            const response = await token.ClientAccountBalance(ctx,);
+            const response = await token.ClientAccountBalance(ctx);
             expect(response).to.equals(1000);
         });
     });
@@ -290,6 +274,25 @@ describe('Chaincode', () => {
             const response = await token.ClientAccountID(ctx);
             sinon.assert.calledOnce(mockClientIdentity.getID);
             expect(response).to.equals('x509::{subject DN}::{issuer DN}');
+        });
+    });
+
+    describe('#ClientAccountMSPID', () => {
+        it('should work', async () => {
+            const response = await token.ClientAccountMSPID(ctx);
+            sinon.assert.calledTwice(mockClientIdentity.getMSPID);
+            expect(response).to.equals('Org1MSP');
+        });
+    });
+
+    describe('#CheckAuthorization', () => {
+        it('should work', async () => {
+            await token.CheckAuthorization(ctx);
+        });
+
+        it('should failed if called by not Org1MSP', () => {
+            mockClientIdentity.getMSPID.returns('Org2MSP');
+            expect(() => token.CheckAuthorization(ctx)).to.throw(Error, 'client is not authorized');
         });
     });
 
