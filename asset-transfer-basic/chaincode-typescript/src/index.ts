5f7ddaeb4aa7644d35ee17c5f3b155fc595b8da8
luc662 <lucas_bonfil@hotmail.com>
2025-04-02 19:45:02 +0000

    se eliminaron applications-gateways inecesarios

--- asset-transfer-basic/chaincode-typescript/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import {type Contract} from 'fabric-contract-api';
-import {AssetTransferContract} from './assetTransfer';
-
-export const contracts: typeof Contract[] = [AssetTransferContract];
