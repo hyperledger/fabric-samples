6b8fcf28c428fc4c5310947438a4d239a54f4a05
FrancoPandolfo <francopandolfo@hotmail.com>
2025-04-08 22:52:48 +0000

    subo cors config

---
+++ asset-transfer-basic/maven-API-SiMeDi/src/main/java/com/code/hyperledger/WebConfig.java
@@ -0,0 +1,24 @@
+package main.java.com.code.hyperledger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173", "http://localhost:3005")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
