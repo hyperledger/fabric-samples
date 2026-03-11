9d12804e0628399354ffa3b629773dfa50d69191
nXtCyberNet <rohantech2005@gmail.com>
2026-03-11 21:18:20 +0530

    fix: correct docker-entrypoint and remove unused testify dep in asset-transfer-go
    Signed-off-by: nXtCyberNet <rohantech2005@gmail.com>

--- full-stack-asset-transfer-guide/contracts/asset-transfer-go/docker/docker-entrypoint.sh
+++ full-stack-asset-transfer-guide/contracts/asset-transfer-go/docker/docker-entrypoint.sh
@@ -9,18 +9,22 @@ if [[ ! -v CHAINCODE_SERVER_ADDRESS ]]; then
     exec ./chaincode --peer.address "${CORE_PEER_ADDRESS}"
 
 elif [[ "${CORE_PEER_TLS_ENABLED,,}" == "true" ]]; then
-    # CaaS + TLS
-    exec ./chaincode \
-        --chaincode.address "${CHAINCODE_SERVER_ADDRESS}" \
-        --chaincode.id     "${CHAINCODE_ID}" \
-        --chaincode.tls.enabled true \
-        --chaincode.tls.key.file    "${CHAINCODE_TLS_KEY:-/hyperledger/privatekey.pem}" \
-        --chaincode.tls.cert.file   "${CHAINCODE_TLS_CERT:-/hyperledger/cert.pem}" \
-        --chaincode.tls.clientCaCert.file "${CHAINCODE_TLS_CLIENT_CACERT:-/hyperledger/rootcert.pem}"
+    # CaaS + TLS: fabric-chaincode-go/v2 reads CHAINCODE_SERVER_ADDRESS,
+    # CORE_CHAINCODE_ID_NAME, and TLS vars directly as env vars.
+    exec env \
+        CORE_CHAINCODE_ID_NAME="${CHAINCODE_ID}" \
+        CHAINCODE_SERVER_ADDRESS="${CHAINCODE_SERVER_ADDRESS}" \
+        CORE_PEER_TLS_ENABLED=true \
+        CORE_PEER_TLS_ROOTCERT_FILE="${CHAINCODE_TLS_KEY:-/hyperledger/privatekey.pem}" \
+        CORE_TLS_CLIENT_KEY_FILE="${CHAINCODE_TLS_CERT:-/hyperledger/cert.pem}" \
+        CORE_TLS_CLIENT_CERT_FILE="${CHAINCODE_TLS_CLIENT_CACERT:-/hyperledger/rootcert.pem}" \
+        ./chaincode
 
 else
-    # CaaS without TLS
-    exec ./chaincode \
-        --chaincode.address "${CHAINCODE_SERVER_ADDRESS}" \
-        --chaincode.id      "${CHAINCODE_ID}"
+    # CaaS without TLS: fabric-chaincode-go/v2 uses CHAINCODE_SERVER_ADDRESS
+    # and CORE_CHAINCODE_ID_NAME env vars to start the gRPC server.
+    exec env \
+        CORE_CHAINCODE_ID_NAME="${CHAINCODE_ID}" \
+        CHAINCODE_SERVER_ADDRESS="${CHAINCODE_SERVER_ADDRESS}" \
+        ./chaincode
 fi
